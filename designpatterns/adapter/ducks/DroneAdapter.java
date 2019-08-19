01c418f43b04a1f90c109adf5cf48e687863b56e
Julian Carachure <jcarachu@outlook.com>
2019-08-19 12:58:54 -0700

    adapting drones to ducks

---
+++ designpatterns/adapter/ducks/DroneAdapter.java
@@ -0,0 +1,23 @@
+package ducks;
+
+public class DroneAdapter implements Duck{
+    Drone drone;
+
+    public DroneAdapter(Drone drone)
+    {
+        this.drone = drone;
+    }
+
+    public void quack()
+    {
+        drone.beep();
+    }
+
+    public void fly()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            drone.take_off();
+        }
+    }
+}
---
+++ designpatterns/adapter/ducks/DuckTestDrive.java
@@ -0,0 +1,25 @@
+package ducks;
+
+public class DuckTestDrive {
+    public static void main(String[] args)
+    {
+        MallardDuck duck = new MallardDuck();
+        WildTurkey turkey = new WildTurkey();
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+        System.out.println("The Turkey says...");
+        turkey.gobble();
+        turkey.fly();
+        System.out.println("\nThe Duck says...");
+        testDuck(turkeyAdapter);
+        Drone drone = new SuperDrone();
+        Duck droneAdapter = new DroneAdapter(drone);
+        testDuck(droneAdapter);
+    }
+
+    static void testDuck(Duck duck)
+    {
+        duck.quack();
+        duck.fly();
+    }
+}
+
